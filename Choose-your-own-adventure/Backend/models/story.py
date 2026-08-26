a43160853bbd2523ce5a602ba7bb796e5ceed39e
rawaldinho2 <atharv.rawal08@gmail.com>
2026-08-26 19:05:23 +0530

    37mins26august

---
+++ Choose-your-own-adventure/Backend/models/story.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, DateTime,Boolean,ForeignKey,JSON
+from sqlalchemy.sql import func 
+from sqlalchemy.orm import relationship
+from db.database import Base
+
+class Story(Base):
+    __tablename__ = "stories" 
+    id = Column(Integer, index=True, primary_key=True)   
