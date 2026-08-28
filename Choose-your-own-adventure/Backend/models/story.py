050524023f004b207a0ee39cdd20766393838006
rawaldinho2 <atharv.rawal08@gmail.com>
2026-08-28 19:49:08 +0530

    meow

--- Choose-your-own-adventure/Backend/models/story.py
+++ Choose-your-own-adventure/Backend/models/story.py
@@ -6,3 +6,13 @@ from db.database import Base
 class Story(Base):
     __tablename__ = "stories" 
     id = Column(Integer, index=True, primary_key=True)   
+    title=Column(String, index=True)
+    session_id=Column (String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    nodes=relationship("StoryNodes", back_populates="story")
+
+    class StoryNode(Base):
+        __tablename__ = "story_nodes"
+
+        id = Column(Integer, primary_key=True, index=True)
