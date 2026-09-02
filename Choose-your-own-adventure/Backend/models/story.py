ee68a3630a449a128c5db43f410f5841f28e3f7f
Atharv Rawal <atharvrawal2007@gmail.com>
2026-09-02 13:42:04 +0000

    Sep 2, 2026, 7:12 PM

--- Choose-your-own-adventure/Backend/models/story.py
+++ Choose-your-own-adventure/Backend/models/story.py
@@ -18,9 +18,9 @@ class Story(Base): #Story Table for the main story (The story from the initial p
         __tablename__ = "story_nodes"
 
         id = Column(Integer, primary_key=True, index=True) 
-        story_id= Column(Integer, ForeignKey("stories.id")) 
+        story_id= Column(Integer, ForeignKey("stories.id")) #Every time someone saves a scene here, look at the stories table's id column. If that story doesn't exist, reject this page.
         content= Column(String)
         is_root= Column(Boolean, default=False) #if this is the first node
         is_ending= Column(Boolean, default=False) # losing endiing node
         is_winning_ending= Column(Boolean, default=False) #if this is the node which wins u the whole adventure 
-        
+
