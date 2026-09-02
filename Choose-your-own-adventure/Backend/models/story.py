3d55461ec76835ab541aaadcb890777bb31b86fe
Atharv Rawal <atharvrawal2007@gmail.com>
2026-09-02 13:36:44 +0000

    Sep 2, 2026, 7:06 PM

--- Choose-your-own-adventure/Backend/models/story.py
+++ Choose-your-own-adventure/Backend/models/story.py
@@ -3,7 +3,9 @@ from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from db.database import Base
 
-class Story(Base):
+#SQL Alchemy creates a link between python and SQL, Using this we can make a SQL database in python using python classes
+
+class Story(Base): #Story Table for the main story (The story from the initial prompt )  
     __tablename__ = "stories" 
     id = Column(Integer, index=True, primary_key=True)   
     title=Column(String, index=True)
@@ -12,8 +14,12 @@ class Story(Base):
 
     nodes=relationship("StoryNodes", back_populates="story")
 
-    class StoryNode(Base):
+    class StoryNode(Base): #Story Nodes are 
         __tablename__ = "story_nodes"
 
         id = Column(Integer, primary_key=True, index=True)
         story_id= Column(Integer, ForeignKey("stories.id"))
+        content= Column(String)
+        is_root= Column(Boolean, default=False)
+        is_root= Column(Boolean, default=False)
+        is_winning_ending= Column(Boolean, default=False)
